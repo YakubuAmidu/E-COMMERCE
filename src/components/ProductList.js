bfe23e640b833c63afeaae2333af8e6a179f309c
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-02 19:55:42 -0700

    Commiting changes

---
+++ src/components/ProductList.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+
+export default class ProductList extends Component {
+  render() {
+    return <div>Hello from Product List</div>;
+  }
+}

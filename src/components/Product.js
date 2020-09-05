9b5758712c5692bff25d437332be980bef119294
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-05 01:25:04 -0700

    Commiting changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,9 +8,20 @@ export default class Product extends Component {
     const { id, title, img, price, inCart } = this.props.product;
 
     return (
-      <div>
-        <h3>Hello from product</h3>
-      </div>
+      <ProductWrapper className='col-9 mx-auto col-md-6 col-lg-3 my-3'>
+        <div className='card'>
+          <div
+            className='img-container p-5'
+            onClick={console.log('You clicked me')}
+          >
+            <Link to='/details'>
+              <img src={img} alt='product' className='card-img-top' />
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;

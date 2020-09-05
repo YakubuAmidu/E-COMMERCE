89c0602d3b8e39429ef169659e73e148f15865de
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-04 23:11:59 -0700

    Commiting changes

--- src/context.js
+++ src/context.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
 
 const ProductContext = React.createContext();
 // Provider
 // Consumer
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct,
+  };
+
   render() {
     return (
-      <ProductContext.Provider value='Hello from context'>
+      <ProductContext.Provider value={{}}>
         {this.props.children}
       </ProductContext.Provider>
     );

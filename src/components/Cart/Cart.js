3a5f2f7bc184d858155d9dd2f6bef4863b36c2ab
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-06 05:11:06 -0700

    Commiting changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import { ProductConsumer } from '../../context';
+import CartList from './CartList';
 
 export default class Cart extends Component {
   render() {
---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartList() {
+  return (
+    <di>
+      Hello from CartList
+      <CartItem />
+    </di>
+  );
+}

147dce2a2e5530469378f3659e19b3b9585c6a68
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-06 01:41:32 -0700

    Commiting changes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,9 +7,30 @@ import { Link } from 'react-router-dom';
 export default class Modal extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from Modal</h1>
-      </div>
+      <ProductConsumer>
+        {(value) => {
+          const { modalOpen, closeModal } = value;
+          const { img, title, price } = value.modalProduct;
+
+          if (!modalOpen) {
+            return null;
+          } else {
+            return (
+              <ModalContainer>
+                <div className='container'>
+                  <div className='row'>
+                    <div className='col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize'>
+                      <h5>Item added to the cart</h5>
+                    </div>
+                  </div>
+                </div>
+              </ModalContainer>
+            );
+          }
+
+          return;
+        }}
+      </ProductConsumer>
     );
   }
 }
